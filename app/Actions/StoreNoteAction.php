94752e3cb087372f54af7efd0a55c0ea93b66753
Clement Grihon <gclems@gmail.com>
2025-09-06 19:28:22 +0200

    fix: can create note

--- app/Actions/StoreNoteAction.php
+++ app/Actions/StoreNoteAction.php
@@ -14,20 +14,12 @@ class StoreNoteAction
         ?int $noteId = null,
         Privacy $privacy = Privacy::Public
     ): Note {
-        $sortOrder = Note::where('campaign_id', $campaignId)
-            ->where('note_category_id', $noteCategoryId)
-            ->where('note_id', $noteId)
-            ->max('sort_order');
-
-        $sortOrder = $sortOrder !== null ? $sortOrder + 1 : 0;
-
         return Note::create([
             'name' => $name,
             'campaign_id' => $campaignId,
             'note_category_id' => $noteCategoryId,
             'note_id' => $noteId,
             'privacy' => $privacy,
-            'sort_order' => $sortOrder,
         ]);
     }
 }
